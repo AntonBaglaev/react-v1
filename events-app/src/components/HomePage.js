a7a1d593e7d1c56ecdfd6cfc7b34c70dadaeb549
Anton Baglaev <abaglaev@yandex.ru>
2025-02-28 15:24:39 +0300

    корректировка главной

--- events-app/src/components/HomePage.js
+++ events-app/src/components/HomePage.js
@@ -4,12 +4,13 @@ import './HomePage.css';
 const HomePage = () => {
   return (
     <div className="home-page">
-      {/* Блок 2: Информация о программе мероприятия */}
-      <div className="event-program">
+      {/* Section: Информация о программе мероприятия */}
+      <section className="event-program">
         <div className="event-program-text">
-          <h1>РАСТИМ ПОКАЗАТЕЛИ ВАШЕГО БИЗНЕСА</h1>
+          <h1>IT HR XAB</h1>
+          <h2>ИСТОЧНИК ВДОХНОВЕНИЯ И ПРОФЕССИОНАЛЬНОГО РОСТА</h2>
           <p>
-            У всех наших BIM менеджеров опыт работы с компаниями более 10 лет. Поэтому мы превращаем наш многолетний опыт BIM в ваш успех!
+            УНИКАЛЬНЫЕ ДОКЛАДЫ, СОДЕРЖАТЕЛЬНЫЕ ПОДКАСТЫ, АНОНСЫ ЗНАЧИМЫХ СОБЫТИЙ И ЦЕННЫЕ МАТЕРИАЛЫ. ОСТАВАЙТЕСЬ В ЦЕНТРЕ HR-ТРЕНДОВ И ДВИГАЙТЕСЬ К УСПЕХУ ВМЕСТЕ С НАМИ!
           </p>
           <div className="event-program-actions">
             <button className="discuss-project-button">Обсудить проект</button>
@@ -19,7 +20,7 @@ const HomePage = () => {
         <div className="event-program-image">
           <img src="https://via.placeholder.com/600x400" alt="Программа мероприятия" />
         </div>
-      </div>
+      </section>
     </div>
   );
 };
--- events-app/src/components/Navbar.js
+++ events-app/src/components/Navbar.js
@@ -4,15 +4,31 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Главная</Link>
-        </li>
-        <li>
-          <Link to="/events">Мероприятия</Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="navbar-container">
+        {/* Логотип */}
+        <div className="navbar-logo">
+          <Link to="/">Логотип</Link>
+        </div>
+
+        {/* Меню */}
+        <ul className="navbar-menu">
+          <li>
+            <Link to="/">Главная</Link>
+          </li>
+          <li>
+            <Link to="/events">Мероприятия</Link>
+          </li>
+          <li>
+            <Link to="/create-event">Создать мероприятие</Link>
+          </li>
+        </ul>
+
+        {/* Номер телефона */}
+        <div className="navbar-phone">
+          <a href="tel:+79999999999">+7 (999) 999-99-99</a>
+        </div>
+      </div>
     </nav>
   );
 };
